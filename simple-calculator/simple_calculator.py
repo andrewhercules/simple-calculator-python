fdbb645e4609d1c35779f52ae68e5bca76201980
Andrew Hercules <andrewhercules@gmail.com>
2017-12-09 16:05:47 +0000

    SimpleCalculator can add two numbers

--- simple-calculator/simple_calculator.py
+++ simple-calculator/simple_calculator.py
@@ -1,1 +1,4 @@
 class SimpleCalculator:
+
+    def add(self, num1, num2):
+        return num1 + num2
--- simple-calculator/test_simple_calculator.py
+++ simple-calculator/test_simple_calculator.py
@@ -1,6 +1,11 @@
 import unittest
+from simple_calculator import SimpleCalculator
 
 class TestSimpleCalculator(unittest.TestCase):
+
+    def test_add(self):
+        calculator = SimpleCalculator()
+        self.assertEqual(calculator.add(2,3), 5)
 
 if __name__ == '__main__':
     unittest.main()
