97b247c056b60ba6f0ec31a86d968f9014d76704
Andrew Hercules <andrewhercules@gmail.com>
2017-12-09 16:12:01 +0000

    Updated test for add() function with assertion that TypeError raised if more than two integer arguments are passed

--- simple-calculator/test_simple_calculator.py
+++ simple-calculator/test_simple_calculator.py
@@ -6,6 +6,8 @@ class TestSimpleCalculator(unittest.TestCase):
     def test_add(self):
         calculator = SimpleCalculator()
         self.assertEqual(calculator.add(2,3), 5)
+        with self.assertRaises(TypeError):
+            calculator.add(2,3,4)
 
 if __name__ == '__main__':
     unittest.main()
